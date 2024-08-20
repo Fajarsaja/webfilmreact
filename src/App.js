be999f9d0f8317eaaceeac83b67146d9829201b6
fajar <ikhromfajar2@gmail.com>
2024-08-20 10:33:52 +0700

    comit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { getMovieList, searchMovie } from './api';
 import './App.css';
 
-function App() {
+const App = () => {
+
+const [ topRatedMovies, setTopRatedMovies] = useState([])
+
+  useEffect(() => {
+    getMovieList().then((result) => {
+      setTopRatedMovies(result)
+    })
+  },[])
+
+  const TopRatedMoviesList = () => {
+    return topRatedMovies.map((movie, i) => {
+      return(
+          <div className='Movie-wrapper' key={i}>
+              <div className='Movie-title'>{movie.title}</div>
+              <img className='Movie-image' src={`${process.env.REACT_APP_BASEIMGURL}/${movie.poster_path}`}/>
+              <div className='Movie-date'>{movie.release_date}</div>
+              <div className='Movie-rate'>{movie.vote_average}</div>
+           </div>
+      )
+    })
+  }
+
+  const search = async (q) => {
+    const query = await searchMovie(q)
+    setTopRatedMovies(query.results)
+    
+  }
+
+  // console.log({  TopRatedMovies : topRatedMovies });
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <div>
+          <h1>FAJAR WEB MOVIE LIST</h1>
+          <input placeholder='cari film disini....'
+                 className='Movie-search'
+                 onChange={({target}) => search(target.value)}>
+          </input>
+          
+          <div className='Movie-container'>
+              <TopRatedMoviesList/>
+          </div>
+       </div>
       </header>
     </div>
   );
